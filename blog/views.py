2c1f5ef0d4d6a194a0327c2f23d0400ea87669d7
Panguins <panguins1337@hotmail.com>
2016-05-14 15:57:44 -0700

    Modified views to display all posts

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	post = Post.objects.filter()
 	return render(request, 'blog/post_list.html', {"posts":post})
 
